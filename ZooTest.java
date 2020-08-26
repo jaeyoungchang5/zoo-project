0ad8a1b2d8d9b06c1277fd519e523bceac3c9a96
jaeyoungchang5 <jchang5@nd.edu>
2020-08-26 16:45:48 -0400

    added functions to print names and print summary

--- ZooTest.java
+++ ZooTest.java
@@ -1,23 +1,5 @@
 public class ZooTest {
-    // public static setAnimals(){
-    //     Animal[] animals = new Animal[6];
-    //     animals[0] = new Wolf("Akela", "least concern", "Indian Wolf", "Male", true, true, true, true);
-    //     animals[1] = new Falcon("Millenium", "least concern", "Falcon", "Male", true, true, "North America", false);
-    //     animals[2] = new Turtle("Crush", "endangered", "Sea Turtle", "Male", true, true, true, true);
-    //     animals[3] = new Lion("Nala", "vulnerable", "South African Lion", "Female", true, true, false, true);
-    //     animals[4] = new Penguin("Kowalski", "near threatened", "Adelie Penguin", "Male", false, false, false, false);
-    //     animals[5] = new Alligator("Swampy", "least concern", "American Alligator", "Male", true, false, false);
-    //     System.out.println("\n");
-    //     return animals;
-    // }
-
-    static void printNames(Animal[] animals) {
-        for (int i = 0; i < animals.length; i++){
-            System.out.println(animals[i].getUniqueName());
-        }
-    }
-
-    public static void main (String[] args){
+    static void setAnimals(){
         Animal[] animals = new Animal[6];
         animals[0] = new Wolf("Akela", "least concern", "Indian Wolf", "Male", true, true, true, true);
         animals[1] = new Falcon("Millenium", "least concern", "Falcon", "Male", true, true, "North America", false);
@@ -26,5 +8,30 @@ public class ZooTest {
         animals[4] = new Penguin("Kowalski", "near threatened", "Adelie Penguin", "Male", false, false, false, false);
         animals[5] = new Alligator("Swampy", "least concern", "American Alligator", "Male", true, false, false);
         printNames(animals);
+        printSummary(animals);
+    }
+
+    static void printNames(Animal[] animals) {
+        for (int i = 0; i < animals.length; i++){
+            System.out.println(animals[i].getUniqueName());
+        }
+    }
+
+    static void printSummary(Animal[] animals) {
+        for (int i = 0; i < animals.length; i++){
+            System.out.println(animals[i].getUniqueName() + " is a " + animals[i].getGender() + " " + animals[i].getAnimalType() + ", whose conservation status is " + animals[i].getConservationStatus());
+        }
+    }
+
+    public static void main (String[] args){
+        // Animal[] animals = new Animal[6];
+        // animals[0] = new Wolf("Akela", "least concern", "Indian Wolf", "Male", true, true, true, true);
+        // animals[1] = new Falcon("Millenium", "least concern", "Falcon", "Male", true, true, "North America", false);
+        // animals[2] = new Turtle("Crush", "endangered", "Sea Turtle", "Male", true, true, true, true);
+        // animals[3] = new Lion("Nala", "vulnerable", "South African Lion", "Female", true, true, false, true);
+        // animals[4] = new Penguin("Kowalski", "near threatened", "Adelie Penguin", "Male", false, false, false, false);
+        // animals[5] = new Alligator("Swampy", "least concern", "American Alligator", "Male", true, false, false);
+        // printNames(animals);
+        setAnimals();
     }
 }
